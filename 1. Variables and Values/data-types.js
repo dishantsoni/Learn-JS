70dfeddc3db5e5fa366586b16c73652b2d70bd78
Dishant Soni <sonidishantv@gmail.com>
2015-12-12 17:03:10 -0600

    Code Example
    Explaining Data Types

---
+++ 1. Variables and Values/data-types.js
@@ -0,0 +1,25 @@
+/** 
+DATA TYPES
+Basic example demonstrating use of primitives in JavaScript.
+
+WHAT TO KNOW?
+1. UNDERSTAND WHAT IS 'var' KEYWORD?
+2. HOW TO DECLARE AND DEFINE A VARIABLE?
+3. LOOSELY TYPED/DYNAMIC TYPED LANGUAGE
+4. DATA TYPES AND VALUES - string, number, boolean, null, undefined, object.
+**/
+
+var vString = "string"; //Use double quotes
+var vStringAnother = 'string'; //Use single quotes
+var vNumber = 10; //Integer like value
+var vNumberAgain = 10.10; //Float like value. No different data types
+var vBoolean = true; //boolean - true OR false
+var vObject = {  //Object literal {} 
+    "name" : "jeo dane",
+    "address" : {
+        "street" : "1234 javascript lane",
+        "zipCode" : 56541
+    }
+};
+var vUndefined; //no value assigned becomes undefined
+var vNull = null; //setting explicit value to null
